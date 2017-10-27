c0cbc90c8a7db0c8e82c9483b592be4840ac0840
hariharanRadhakrishnan <coolhari1996@gmail.com>
2017-10-27 09:56:41 +0530

    Face Detection and Facial Points

--- faceDetection/openCV_detection.py
+++ faceDetection/openCV_detection.py
@@ -1,31 +1,55 @@
 import numpy as np
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
 import cv2
 import copy
+
+def face_points(gray):
+    b,r = gray.shape[:2]
+    rect = dlib.rectangle(left=0,top=0,right=r,bottom=b)
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+    (x, y, w, h) = face_utils.rect_to_bb(rect)
+    for (x, y) in shape:
+        cv2.circle(gray, (x, y), 1, (0, 0, 255), -1)
+    return gray
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img = cv2.imread('../images/image4.jpg')
+img = cv2.imread('../images/image1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
 cropped_faces = []
 for (x,y,w,h) in faces:
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = copy.copy(img[y:y+h, x:x+w])
+    roi_color = img[y:y+h, x:x+w]
     cropped_faces.append(roi_color)
     cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+
 
 count = 0
 cv2.imshow('Detected Images',gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
 scale = 200
+
 for i in cropped_faces:
     height, width = i.shape[:2]
     scale_factor = width/scale
     scale_x = int(width/scale_factor)
     scale_y = int(height/scale_factor)
+    i = face_points(i)
     i = cv2.resize(i,(scale_x,scale_y))
     cv2.imshow('img'+str(count),i)
     cv2.waitKey(0)
     count += 1
+
+cv2.imshow('initail image',img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
