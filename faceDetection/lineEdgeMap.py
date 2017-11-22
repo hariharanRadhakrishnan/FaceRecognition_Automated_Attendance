907cbef5d20680de0c1f0bf1eebd9a861baf97b3
Sandeep Pvn <sandeeppvn@gmail.com>
2017-11-23 00:39:24 +0530

    Easiest Hausdroff working, yet to implement skew,tilt,dimenion check etc. Detection yet to be perfected

--- faceDetection/lineEdgeMap.py
+++ faceDetection/lineEdgeMap.py
@@ -8,12 +8,8 @@ import dlib
 import cv2
 import copy
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-img = cv2.imread('../images/image9.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+
 
 def dist(a,b):
     return ( (a[0]-b[0])**2 + (a[1]-b[1])**2)*(1/2.0)
@@ -58,11 +54,18 @@ def hausdroff(img_features,temp_features):
     return total
 
 def recognize(img_features):
+    all_features = []
+    for feature in img_features:
+        for point in feature:
+            all_features.append(point.tolist())
+    print(all_features)
+    '''
     threshold = ?
     min_d = 9999
     index = 0
     for i,temp_features in enumerate(each_template_from_db):
-        dist = hausdroff(img_features, temp_features)
+        # dist = hausdroff(img_features, temp_features)
+        dist = hausdroff_distance(all_features,temp_all_points)
         if(dist < min_d):
             min_d = dist
             index = i
@@ -70,8 +73,29 @@ def recognize(img_features):
         return index
     else:
         return -1
+    '''
+
+def skin_mask(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #static skin color threshold
+    hueLower = 0
+    hueHigher = 100
+
+    #global skin color threshold
+    lower = np.array([hueLower,10,10])
+    higher = np.array([hueHigher,255,255])
+
+    #create skin mask
+    mask = cv2.inRange(hsv,lower,higher)
+
+    #Bitwise-AND mask and original image
+    res = cv2.bitwise_and(img,img,mask=mask)
+    cv2.imshow('Mask Res Images',img)
+
+    cv2.waitKey(0)
 
 def face_points(gray):
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
     b,r = gray.shape[:2]
     rect = dlib.rectangle(left=0,top=0,right=r,bottom=b)
     shape = predictor(gray, rect)
@@ -104,36 +128,63 @@ def getLEM(shape,count):
     root.geometry("200x200+300+300")
     root.mainloop()
 
-def main():
+def detect_faces():
+
+    face_cascade = cv2.CascadeClassifier('cascades\haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('cascades\haarcascade_eye.xml')
+
+    img = cv2.imread('../images/sandeep_far.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+
     cropped_faces = []
     for (x,y,w,h) in faces:
-        roi_color = copy.copy(img[y-5:y+5+h, x-5:x+5+w])
-        cropped_faces.append(roi_color)
-        cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_color = copy.copy(img[y:y+h, x:x+w])
+        roi_gray = copy.copy(gray[y:y+h, x:x+w])
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if(len(eyes)>=0):
+            # print(eyes)
+            cropped_faces.append(roi_color)
+            cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+    
 
 
+    scale = 1000
+    height, width = gray.shape[:2]
+    scale_factor = width/scale
+    scale_x = int(width/scale_factor)
+    scale_y = int(height/scale_factor)
     count = 0
-    #cv2.imshow('Detected Images',gray)
+    gray = cv2.resize(gray,(scale_x,scale_y))
+    cv2.imshow('Detected Images',gray)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    scale = 200
+    return cropped_faces
 
+def main():
+    
+    count = 0
+    cropped_faces= detect_faces()
+    scale = 200
+    
     for i in cropped_faces:
         height, width = i.shape[:2]
         scale_factor = width/scale
         scale_x = int(width/scale_factor)
         scale_y = int(height/scale_factor)
         i = cv2.resize(i,(scale_x,scale_y))
-        i,features = face_points(i)
-        cv2.imshow('img'+str(count),i)
+
+        feature_img,features = face_points(i)
+        cv2.imshow('img'+str(count),feature_img)
 
         index = recognize(features)
 
-        print(database[index])
+        #print(database[index])
         #getLEM(shape, count)
         cv2.waitKey(0)
-        cv2.destroyAllWindows()
         count += 1
+    cv2.destroyAllWindows()
+    
 
 main()
