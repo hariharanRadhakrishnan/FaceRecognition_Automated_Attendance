635aae62438040dc00299af16769f3b36e554183
Sandeep Pvn <sandeeppvn@gmail.com>
2017-11-23 14:55:49 +0530

    Database to be created, LEM to be updated, Skew tilt to be done yet(verenoi tessalations)

---
+++ faceDetection/updateDB.py
@@ -0,0 +1,95 @@
+import numpy as np
+from tkinter import Tk, Canvas, Frame, BOTH
+from imutils import face_utils
+import imutils
+from detectSkinColor import onlyFace
+import numpy as np
+import dlib
+import cv2
+import copy
+
+
+#Face points for one image only
+def face_points(gray):
+    predictor = dlib.shape_predictor("facial-landmarks/shape_predictor_68_face_landmarks.dat")
+    b,r = gray.shape[:2]
+    rect = dlib.rectangle(left=0,top=0,right=r,bottom=b)
+    points = predictor(gray, rect)
+    points = face_utils.shape_to_np(points)
+    
+    for (x, y) in points:
+        cv2.circle(gray, (x, y), 1, (0, 0, 255), -1)
+
+        
+    return gray,points.tolist()
+
+#Detect all images
+def detect(img,gray):
+    #Detect Faces
+    cropped_faces = []
+    face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
+    faces = face_cascade.detectMultiScale(gray)
+    for (x,y,w,h) in faces:
+        roi_color = copy.copy(img[y-5:y+5+h, x-5:x+5+w])
+        roi_gray = copy.copy(gray[y:y+h, x:x+w])
+        eyes = []
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if(len(eyes)):
+            cropped_faces.append(roi_color)
+            cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+
+    count = 0
+    cv2.imshow('Detected Images',gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return cropped_faces
+
+#For all detected images, store the (feature image,shape)
+def get_points(cropped_faces):
+
+    features = []
+    count = 0
+    for i in cropped_faces:
+    	#Scale the image to needed size
+        i = imutils.resize(i,width=200)                                 
+
+        #Detect/isolate image based on skin color
+        i = onlyFace(i)                                                 
+
+        #Detect all feature points(68) 
+        shape_img,points = face_points(i)  
+
+        #Display image with points           
+        cv2.imshow('img'+str(count),shape_img)                          
+        count += 1
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        
+        features.append(points)
+   
+    return features
+   
+
+def process(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #Detect thee faces in a picture
+    cropped_faces = detect(img,gray)
+
+    #Obtain points/andmarks for each detected face
+    points_set = get_points(cropped_faces)
+
+    for points in points_set:
+    	#Input name and update points and name into a list into a file
+    	print("points")
+
+def main():
+	path='../images/img ('
+	for i in range(5,13):
+		full_path = path + str(i) +").jpg"
+		img = cv2.imread(full_path)
+		process(img)
+
+main()
---
+++ faceDetection/updateLEM.py
@@ -0,0 +1,238 @@
+import numpy as np
+from tkinter import Tk, Canvas, Frame, BOTH
+from imutils import face_utils
+import imutils
+from detectSkinColor import onlyFace
+import numpy as np
+import dlib
+import cv2
+import copy
+
+
+
+
+
+file = ''
+# file = open("line_edge_maps.csv",'a')
+
+def dist(a,b):
+    return dist_scaled(a,b)#+dist_tilted(a,b)+dist_skewed(a,b)
+
+#This is the distance measure adjusted to a frame of reference for scaled image
+def dist_scaled(a,b):
+    new_a = ( (a[0]-ref_a[0])**2 + (a[1]-ref_a[1])**2)*(1/2.0)
+    new_b = ( (b[0]-ref_b[0])**2 + (b[1]-ref_b[1])**2)*(1/2.0)
+
+    return abs(new_a-new_b)
+
+#This is the distance measure adjusted to a frame of reference for tilted image
+def dist_tilted(a,b):
+    pass
+#This is the distance measure adjusted to a frame of reference for skewed image
+def dist_skewed(a,b):
+    pass
+
+#Compute generic hausdrauff distance
+def hausdroff_distance(set_a,set_b):
+
+    #Calculate min from a -> b
+    hausdrauff_dist_a = 0
+    hausdrauff_pts_a=[]
+    for a in set_a:
+        min_d = 9999
+        min_pts = []
+        for b in set_b:
+            distance = dist(a,b)
+            if(distance<min_d):
+                min_d=distance
+                min_pts=a,b
+        if(min_d>hausdrauff_dist_a):
+            hausdrauff_dist_a=min_d
+            hausdrauff_pts_a=min_pts
+
+    #Calculate min from b -> a
+    hausdrauff_dist_b = 0
+    hausdrauff_pts_b=[]
+    for b in set_b:
+        min_d = 9999
+        min_pts = []
+        for a in set_a:
+            distance = dist(b,a)
+            if(distance<min_d):
+                min_d=distance
+                min_pts=b,a
+        if(min_d>hausdrauff_dist_b):
+            hausdrauff_dist_b=min_d
+            hausdrauff_pts_b=min_pts
+
+    return  max(hausdrauff_dist_a,hausdrauff_dist_b)
+
+#Compute Hausdrauff distance for all points,and also shape specific
+def hausdroff(test_points,temp_points):
+
+    #OPTION 1: compute Hausdrauff distance for all points togethor
+    return hausdroff_distance(test_points,temp_points)
+
+    '''
+
+    #OPTION 2: compute Hausdrauff distance for individual shape feature and sum it, (add weightage later)
+    shapes = ['face_curve','left_eyebro','right_eyebro','nose','left_eye','right_eye','mouth']
+    temp_shape = build_shape(temp_points)
+    test_shape = build_shape(test_points)
+
+    total = 0
+    for i,shape_name in enumerate(shapes):
+        #Use the weightage for the classifiers here for each feature
+        total += hausdroff_distance(test_shape[i],temp_shape[i])
+    return total
+    
+
+    #OPTION 3: compute LEM and then do line hausdroff distance
+    test_LEM = getLEM(test_shape)
+    temp_LEM = getLEM(temp_shape)
+
+    return line_hausdrauff_distance(test_LEM,temp_LEM)  
+
+    '''
+
+#Using the 68 landmark points identify the shape of the face
+def build_shape(points):
+
+    face_curve = shape[:17]
+    left_eyebro = shape[17:22]
+    right_eyebro = shape[22:27]
+    nose = shape[27:36]
+    left_eye = shape[36:42]
+    right_eye = shape[42:48]
+    mouth = shape[48:]
+
+    shape = face_curve,left_eyebro,right_eyebro,nose,left_eye,right_eye,mouth
+
+#Face points for one image only
+def face_points(gray):
+    predictor = dlib.shape_predictor("facial-landmarks/shape_predictor_68_face_landmarks.dat")
+    b,r = gray.shape[:2]
+    rect = dlib.rectangle(left=0,top=0,right=r,bottom=b)
+    points = predictor(gray, rect)
+    points = face_utils.shape_to_np(points)
+    
+    for (x, y) in points:
+        cv2.circle(gray, (x, y), 1, (0, 0, 255), -1)
+
+        
+    return gray,points.tolist()
+
+#Detect all images
+def detect(img,gray):
+    #Detect Faces
+    cropped_faces = []
+    face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
+    faces = face_cascade.detectMultiScale(gray)
+    for (x,y,w,h) in faces:
+        roi_color = copy.copy(img[y-5:y+5+h, x-5:x+5+w])
+        roi_gray = copy.copy(gray[y:y+h, x:x+w])
+        eyes = []
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if(len(eyes)):
+            cropped_faces.append(roi_color)
+            cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+
+    count = 0
+    # cv2.imshow('Detected Images',gray)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return cropped_faces
+
+#For all detected images, store the (feature image,shape)
+def get_points(cropped_faces):
+
+    features = []
+   
+    for i in cropped_faces:
+
+        i = imutils.resize(i,width=200)                                 #Scale the image to needed size
+
+        i = onlyFace(i)                                                 #Detect/isolate image based on skin color
+
+        shape_img,points = face_points(i)                               #Detect all feature points(68) 
+        cv2.imshow('img'+str(count),shape_img)                          #Display image with points
+        count += 1
+        cv2.waitKey(0)
+
+        
+        
+        features.append(points)
+   
+    return features
+   
+#Given a points set for a face, compute their hausdroff distance with all points set of faces in a DB and return the best match name
+def recognize(test_points):
+    hausdroff_list=[]
+
+    #For each template in the database,compare test_feature
+    #A template contains name and points corresponsding to it
+    for template in database:
+        #template[0] is name a, template[1] is 68 landmark points
+        name,template_points = template
+        hausdroff_list.append(hausdroff(template_points,test_points),name)
+
+
+
+    #hausdroff_list is list of hausdroff values for each temple with the test image
+    #Get the name,hausdrauff_dist for min with respect to first value in this list
+    #Here value can either be Hausdroff distance of 1.All points togethor, 2. Weighted summation of each shape, 3.Line Hausdrauff Distance
+    value,name = min(hausdroff_list)
+
+
+
+    #If the min value is below a threshold only then return the name
+    if(value<threshold):
+        return name
+
+    else:
+        return "Not Found"
+   
+#LEM for one image only
+def getLEM(shape,count):
+    root = Tk()
+    f = Frame()
+    f.master.title("Lines "+str(count))
+    f.pack(fill=BOTH, expand=1)
+    canvas = Canvas(f)
+    for s in shape:
+        for i in range(1,len(s)):
+            canvas.create_line(s[i-1][0], s[i-1][1], s[i][0], s[i][1])
+        canvas.create_line(s[len(s)-1][0], s[len(s)-1][1], s[0][0], s[0][1])
+    canvas.pack(fill=BOTH, expand=1)
+    root.geometry("200x200+300+300")
+    root.mainloop()
+
+#Starting point: Main Function
+def main():
+    img = cv2.imread('../images/sandeep_far.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #Detect thee faces in a picture
+    cropped_faces = detect(img,gray)
+
+    #Obtain points/andmarks for each detected face
+    points_set = get_points(cropped_faces)
+
+    #For each detected and landmarked face, Do
+    for points in points_set:
+        #A points set is defined by 68 landmark points
+        
+        #Draw the LEM for the feature, recognize can be done inside
+        #getLEM(shape, count)                                            
+
+        #Recignize the feature from DB
+        recognized_name = recognize(points)
+
+        #Display the name of the recognized feature
+        print(recognized_name)
+
+    cv2.destroyAllWindows()
+
+
+main()
