fdfa263ce970276093c4a39d14c00f5ee4bba358
Sandeep Pvn <sandeeppvn@gmail.com>
2017-11-26 20:37:12 +0530

    Version of voronoi for cv3

--- CV project/delaunay.py
+++ CV project/delaunay.py
@@ -4,6 +4,14 @@ import cv2
 import numpy as np
 import random
 
+
+'''
+Prateek : Sandeep CV version
+cv2.CV_AA : cv2.LINE_AA
+cv2.cv : only cv2
+CV_FILLED : FILLED
+'''
+
 # Check if a point is inside a rectangle
 def rect_contains(rect, point) :
     if point[0] < rect[0] :
@@ -18,7 +26,7 @@ def rect_contains(rect, point) :
 
 # Draw a point
 def draw_point(img, p, color ) :
-    cv2.circle( img, p, 2, color, cv2.cv.CV_FILLED, cv2.CV_AA, 0 )
+    cv2.circle( img, p, 2, color, cv2.FILLED, cv2.LINE_AA, 0 )
 
 
 # Draw delaunay triangles
@@ -37,9 +45,13 @@ def draw_delaunay(img, subdiv, delaunay_color ) :
 
         if rect_contains(r, pt1) and rect_contains(r, pt2) and rect_contains(r, pt3) :
 
-            cv2.line(img, pt1, pt2, delaunay_color, 1, cv2.CV_AA, 0)
-            cv2.line(img, pt2, pt3, delaunay_color, 1, cv2.CV_AA, 0)
-            cv2.line(img, pt3, pt1, delaunay_color, 1, cv2.CV_AA, 0)
+          
+
+            #For Sandeep CV version
+            cv2.line(img, pt1, pt2, delaunay_color, 1, cv2.LINE_AA, 0)
+            cv2.line(img, pt2, pt3, delaunay_color, 1, cv2.LINE_AA, 0)
+            cv2.line(img, pt3, pt1, delaunay_color, 1, cv2.LINE_AA, 0)
+
 
 
 # Draw voronoi diagram
@@ -48,7 +60,7 @@ def draw_voronoi(img, subdiv) :
     ( facets, centers) = subdiv.getVoronoiFacetList([])
     
 
-    for i in xrange(0,len(facets)) :
+    for i in range(0,len(facets)) :
         ifacet_arr = []
         for f in facets[i] :
             ifacet_arr.append(f)
@@ -56,10 +68,10 @@ def draw_voronoi(img, subdiv) :
         ifacet = np.array(ifacet_arr, np.int)
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-        cv2.fillConvexPoly(img, ifacet, color, cv2.CV_AA, 0);
+        cv2.fillConvexPoly(img, ifacet, color, cv2.LINE_AA, 0);
         ifacets = np.array([ifacet])
-        cv2.polylines(img, ifacets, True, (0, 0, 0), 1, cv2.CV_AA, 0)
-        cv2.circle(img, (centers[i][0], centers[i][1]), 3, (0, 0, 0), cv2.cv.CV_FILLED, cv2.CV_AA, 0)
+        cv2.polylines(img, ifacets, True, (0, 0, 0), 1, cv2.LINE_AA, 0)
+        cv2.circle(img, (centers[i][0], centers[i][1]), 3, (0, 0, 0), cv2.FILLED, cv2.LINE_AA, 0)
 
 
 if __name__ == '__main__':
@@ -77,6 +89,13 @@ if __name__ == '__main__':
 
     # Read in the image.
     img = cv2.imread("obama.jpg");
+
+    # scale = 190
+    # height, width= img.shape[:2]
+    # scale_factor = width/scale
+    # scale_x = int(width/scale_factor)
+    # scale_y = int(height/scale_factor)
+    # img = cv2.resize(img, (200, 230))
 
     # Keep a copy around
     img_orig = img.copy();
