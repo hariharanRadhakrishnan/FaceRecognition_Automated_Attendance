a83f86be4eb660c53b3359a3d8e65bb00a748279
Hari <coolhari1996@gmail.com>
2017-12-03 16:57:00 +0530

    Revert "Comma seperated points to lines"
    This reverts commit b1a4c3d97d1027c2928f3dc4c52f0a4041bf5d2d.

--- faceRecognition/convertToLines.py
+++
@@ -1,9 +0,0 @@
-def convert(s):
-	s = s.split(",")
-	s = [[int(x) for x in i.split()] for i in s]
-	lineSet = []
-	for i in range(1,len(s)):
-		lineSet.append([s[i-1],s[i]])
-	return lineSet
-
-print(convert(input()))
